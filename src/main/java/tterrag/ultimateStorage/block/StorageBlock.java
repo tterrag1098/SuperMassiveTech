48633f77a1e4f65879143740ee7f424779ec8e15
scorpianz1525 <jacquesrascagneres@gmail.com>
2014-02-18 23:53:56 +0000

    Localised the name + textures done :)

--- src/main/java/tterrag/ultimateStorage/block/StorageBlock.java
+++ src/main/java/tterrag/ultimateStorage/block/StorageBlock.java
@@ -5,12 +5,20 @@
  */
 package tterrag.ultimateStorage.block;
 
+import tterrag.ultimateStorage.lib.*;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import tterrag.ultimateStorage.UltimateStorage;
 import tterrag.ultimateStorage.tile.TileStorageBlock;
@@ -57,5 +65,39 @@ public class StorageBlock extends BlockContainer
 			return true;
 		}
 		return false;
+	}
+	
+
+	@SideOnly(Side.CLIENT)
+	private IIcon side, bottom, top, front;
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister reg)
+	{
+		this.side = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + (this.getUnlocalizedName().substring(8)) + "sides");
+		this.bottom = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + (this.getUnlocalizedName().substring(8)) + "top");
+		this.top = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + (this.getUnlocalizedName().substring(8)) + "top");
+		this.front = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + (this.getUnlocalizedName().substring(8)) + "front");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public IIcon getIcon(int side, int metadata)
+	{
+		if (side == 1) return this.top;
+		else if (side == 0) return this.top;
+		else if (metadata == 2 && side == 2) return this.front;
+		else if (metadata == 3 && side == 5) return this.front;
+		else if (metadata == 0 && side == 3) return this.front;
+		else if (metadata == 1 && side == 4) return this.front;
+		else return this.side;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+	{
+		int whichDirectionFacing = MathHelper.floor_double(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F + 2.5D) & 3;
+		par1World.setBlockMetadataWithNotify(x, y, z, whichDirectionFacing, 2);
 	}
 }
--- src/main/java/tterrag/ultimateStorage/lib/Reference.java
+++ src/main/java/tterrag/ultimateStorage/lib/Reference.java
@@ -3,6 +3,7 @@ package tterrag.ultimateStorage.lib;
 public class Reference {
 	
 	public static final String MODID = "ultimateStorage";
+	public static final String MOD_TEXTUREPATH = "ultimatestorage";
 	public static final String MOD_NAME = "Ultimate Storage";
 	public static final String VERSION = "0.0.1";
 
