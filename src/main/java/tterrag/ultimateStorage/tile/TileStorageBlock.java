3740968fbb000fd22cc829f55f3ecfff4cc5ce94
scorpianz1525 <jacquesrascagneres@gmail.com>
2014-02-18 19:24:27 +0000

    NBT!

--- src/main/java/tterrag/ultimateStorage/tile/TileStorageBlock.java
+++ src/main/java/tterrag/ultimateStorage/tile/TileStorageBlock.java
@@ -259,18 +259,8 @@ public class TileStorageBlock extends TileEntity implements ISidedInventory
 		System.out.println("Read");
 		System.out.println(stored);
 		System.out.println(storedItems);
+		this.readFromNBT(nbt);
 	}
 	
-	@Override
-	public Packet getDescriptionPacket(){
-		NBTTagCompound nbt = new NBTTagCompound();
-		this.writeToNBT(nbt);
-		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbt);
-	}
-	
-	@Override
-	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
-	{
-		this.readFromNBT(pkt.func_148857_g());
-	}
+
 }
