1fd4d2dca15d82e536500b28402ee854d5438e48
tterrag1098 <tterrag1098@gmail.com>
2014-03-01 19:13:30 -0500

    Version bump for first build

--- src/main/java/tterrag/ultimateStorage/lib/Reference.java
+++ src/main/java/tterrag/ultimateStorage/lib/Reference.java
@@ -5,6 +5,6 @@ public class Reference {
 	public static final String MODID = "ultimateStorage";
 	public static final String MOD_TEXTUREPATH = "ultimatestorage";
 	public static final String MOD_NAME = "Ultimate Storage";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 
 }
