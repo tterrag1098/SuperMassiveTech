d9364e7297fe92a72497df93ea87041bbda5aaa4
tterrag1098 <tterrag1098@gmail.com>
2014-03-07 17:03:15 -0500

    Fix motion of entityItem

--- src/main/java/tterrag/supermassivetech/entity/EntityStarHeart.java
+++ src/main/java/tterrag/supermassivetech/entity/EntityStarHeart.java
@@ -3,43 +3,63 @@ package tterrag.supermassivetech.entity;
 import java.util.Random;
 
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 import tterrag.supermassivetech.SuperMassiveTech;
 import tterrag.supermassivetech.item.ItemStar.StarType;
 import tterrag.supermassivetech.util.Utils;
 
-public class EntityStarHeart extends EntityItem {
-	
-	public EntityStarHeart(World world, double posX, double posY, double posZ,
-			ItemStack itemstack) {
+public class EntityStarHeart extends EntityItem
+{
+
+	public EntityStarHeart(World world, double posX, double posY, double posZ, ItemStack itemstack, double motionX, double motionY, double motionZ, int delay)
+	{
 		super(world, posX, posY, posZ, itemstack);
+		this.motionX = motionX;
+		this.motionY = motionY;
+		this.motionZ = motionZ;
 		this.isImmuneToFire = true;
-		this.delayBeforeCanPickup = 40;
+		this.delayBeforeCanPickup = delay;
 	}
 
 	@Override
-	public void onUpdate() {
+	public void onUpdate()
+	{
 		super.onUpdate();
-		
+
 		if (this.isBurning())
 		{
-			//changeToStar();
+			changeToStar();
 		}
 	}
 
-	private void changeToStar() {
+	private void changeToStar()
+	{
 		ItemStack star = new ItemStack(SuperMassiveTech.itemRegistry.star, this.getEntityItem().stackSize);
-		
+
 		// Sets the type of the star to a random type
 		Utils.setType(star, StarType.values()[new Random().nextInt(StarType.values().length)]);
-		
+
+		worldObj.setBlock((int) posX, (int) posY, (int) posZ, Blocks.air);
 		worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ));
 		this.setDead();
 	}
-	
+
 	@Override
-	public boolean isEntityInvulnerable() {
+	public boolean isEntityInvulnerable()
+	{
 		return true;
+	}
+
+	@Override
+	public boolean attackEntityFrom(DamageSource par1DamageSource, float par2)
+	{
+		if (par1DamageSource.isFireDamage())
+			return false;
+
+		else
+			return super.attackEntityFrom(par1DamageSource, par2);
 	}
 }
--- src/main/java/tterrag/supermassivetech/item/ItemStarHeart.java
+++ src/main/java/tterrag/supermassivetech/item/ItemStarHeart.java
@@ -1,9 +1,10 @@
 package tterrag.supermassivetech.item;
 
-import tterrag.supermassivetech.entity.EntityStarHeart;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import tterrag.supermassivetech.entity.EntityStarHeart;
 
 public class ItemStarHeart extends ItemSMT {
 
@@ -14,7 +15,7 @@ public class ItemStarHeart extends ItemSMT {
 	@Override
 	public Entity createEntity(World world, Entity location, ItemStack itemstack) {
 		
-		return new EntityStarHeart(world, location.posX, location.posY, location.posZ, itemstack);
+		return new EntityStarHeart(world, location.posX, location.posY, location.posZ, itemstack, location.motionX, location.motionY, location.motionZ, ((EntityItem)location).delayBeforeCanPickup);
 	}
 	
 	@Override
