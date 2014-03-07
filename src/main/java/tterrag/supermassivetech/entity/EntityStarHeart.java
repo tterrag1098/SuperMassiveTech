c9704df41287a9d37e913063992f5b1cbe006b30
tterrag1098 <tterrag1098@gmail.com>
2014-03-07 18:05:27 -0500

    Finish transformation from heart > star

--- src/main/java/tterrag/supermassivetech/entity/EntityStarHeart.java
+++ src/main/java/tterrag/supermassivetech/entity/EntityStarHeart.java
@@ -2,6 +2,7 @@ package tterrag.supermassivetech.entity;
 
 import java.util.Random;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
@@ -10,10 +11,10 @@ import net.minecraft.world.World;
 import tterrag.supermassivetech.SuperMassiveTech;
 import tterrag.supermassivetech.item.ItemStar.StarType;
 import tterrag.supermassivetech.util.Utils;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
 
 public class EntityStarHeart extends EntityItem
 {
-
 	public EntityStarHeart(World world, double posX, double posY, double posZ, ItemStack itemstack, double motionX, double motionY, double motionZ, int delay)
 	{
 		super(world, posX, posY, posZ, itemstack);
@@ -42,8 +43,17 @@ public class EntityStarHeart extends EntityItem
 		// Sets the type of the star to a random type
 		Utils.setType(star, StarType.values()[new Random().nextInt(StarType.values().length)]);
 
-		worldObj.setBlock((int) posX, (int) posY, (int) posZ, Blocks.air);
-		worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ));
+		System.out.println(((int) posX) + " " + ((int) posY) + " " + ((int) posZ));
+		if (worldObj.getBlock((int) posX - 1, (int) posY, (int) posZ) == Blocks.fire)
+		{
+			worldObj.setBlockToAir((int) posX - 1, (int) posY, (int) posZ);
+		}
+		else if (worldObj.getBlock((int) posX - 1, (int) posY - 1, (int) posZ) == Blocks.fire)
+		{
+			worldObj.setBlockToAir((int) posX - 1, (int) posY - 1, (int) posZ);
+		}
+
+		worldObj.spawnEntityInWorld(new EntityItem(worldObj, posX, posY, posZ, star));
 		this.setDead();
 	}
 
@@ -51,6 +61,14 @@ public class EntityStarHeart extends EntityItem
 	public boolean isEntityInvulnerable()
 	{
 		return true;
+	}
+	
+	@Override
+	public boolean isBurning()
+	{
+		boolean flag = this.worldObj != null && this.worldObj.isRemote;
+		int fire = ObfuscationReflectionHelper.getPrivateValue(Entity.class, this, "fire", "field_70151_c");
+        return (fire > 0 || flag && this.getFlag(0));
 	}
 
 	@Override
