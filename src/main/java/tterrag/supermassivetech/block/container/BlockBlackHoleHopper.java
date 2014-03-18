0587df598886a3591cfa02e13798c644c85fbc99
tterrag1098 <tterrag1098@gmail.com>
2014-03-18 16:21:59 -0400

    Add support for hopper configuration (finally!) 	- Configure by right clicking with an item 	- Check the configuration by right clicking with empty hand 	- Clear configuration by shift right clicking with an empty hand

--- src/main/java/tterrag/supermassivetech/block/container/BlockBlackHoleHopper.java
+++ src/main/java/tterrag/supermassivetech/block/container/BlockBlackHoleHopper.java
@@ -1,6 +1,9 @@
 package tterrag.supermassivetech.block.container;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.Facing;
 import net.minecraft.world.World;
 import tterrag.supermassivetech.SuperMassiveTech;
@@ -40,5 +43,30 @@ public class BlockBlackHoleHopper extends BlockContainerSMT
 	public int getRenderType()
 	{
 		return SuperMassiveTech.renderIDHopper;
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) 
+	{
+		TileBlackHoleHopper te = (TileBlackHoleHopper) world.getTileEntity(x, y, z);
+		
+		if (te == null || world.isRemote)
+			return true;
+		
+		ItemStack current = player.getCurrentEquippedItem();
+		
+		if (current == null)
+		{
+			if (player.isSneaking())
+				te.clearConfig(player);
+			else
+				player.addChatMessage(new ChatComponentText("Current Configuration: " + te.getConfig()));	
+		}
+		else
+		{
+			te.setConfig(current, player);
+		}
+		
+		return true;
 	}
 }
--- src/main/java/tterrag/supermassivetech/tile/TileBlackHoleHopper.java
+++ src/main/java/tterrag/supermassivetech/tile/TileBlackHoleHopper.java
@@ -3,19 +3,24 @@ package tterrag.supermassivetech.tile;
 import java.util.ArrayList;
 import java.util.List;
 
-import cpw.mods.fml.client.FMLClientHandler;
-import cpw.mods.fml.common.FMLCommonHandler;
-
 import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.StatCollector;
 import net.minecraftforge.common.util.ForgeDirection;
 import tterrag.supermassivetech.SuperMassiveTech;
 import tterrag.supermassivetech.network.packet.PacketHopperParticle;
+import cpw.mods.fml.client.FMLClientHandler;
 
 public class TileBlackHoleHopper extends TileSMT
 {
@@ -49,7 +54,6 @@ public class TileBlackHoleHopper extends TileSMT
 	public TileBlackHoleHopper()
 	{
 		super(1, 0.8f);
-		inventory[cfgSlot] = new ItemStack(Blocks.dirt);
 	}
 
 	@Override
@@ -155,7 +159,7 @@ public class TileBlackHoleHopper extends TileSMT
 			{
 				ItemStack stack = i.inv.getStackInSlot(idx);
 
-				if (stack != null && stack.getItem() == inventory[cfgSlot].getItem() && stack.stackSize > 0
+				if (stack != null && inventory[cfgSlot] != null && stack.getItem() == inventory[cfgSlot].getItem() && stack.stackSize > 0
 						&& (inventory[hiddenSlot] == null || inventory[hiddenSlot].stackSize < inventory[hiddenSlot].getMaxStackSize()))
 				{
 					i.inv.decrStackSize(idx, 1);
@@ -271,6 +275,29 @@ public class TileBlackHoleHopper extends TileSMT
 	{
 		return te1.xCoord != te2.x || te1.yCoord != te2.y || te1.zCoord != te2.z;
 	}
+	
+	public void setConfig(ItemStack stack, EntityPlayer player)
+	{
+		player.addChatMessage(new ChatComponentText("Set Configuration: " + StatCollector.translateToLocal(stack.getUnlocalizedName() + ".name")));
+		inventory[cfgSlot] = stack.copy();
+	}
+	
+	public String getConfig()
+	{
+		return inventory[cfgSlot] == null ? "nothing" : StatCollector.translateToLocal(inventory[cfgSlot].getUnlocalizedName() + ".name");
+	}
+	
+	public void clearConfig(EntityPlayer player)
+	{
+		if (inventory[cfgSlot] == null)
+		{
+			player.addChatMessage(new ChatComponentText("No Configuration Set!"));
+			return;
+		}
+		
+		player.addChatMessage(new ChatComponentText("Cleared Configuration: " + StatCollector.translateToLocal(inventory[cfgSlot].getUnlocalizedName() + ".name")));
+		inventory[cfgSlot] = null;
+	}
 
 	public static boolean itemStackEquals(ItemStack s1, ItemStack s2)
 	{
@@ -286,5 +313,55 @@ public class TileBlackHoleHopper extends TileSMT
 			return false;
 
 		return s1.getTagCompound().equals(s2.getTagCompound());
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound nbt)
+	{
+		super.writeToNBT(nbt);
+
+		NBTTagList nbttaglist = new NBTTagList();
+
+		for (int i = 0; i < this.inventory.length; ++i)
+		{
+			if (this.inventory[i] != null)
+			{
+				NBTTagCompound nbttagcompound1 = new NBTTagCompound();
+				nbttagcompound1.setByte("Slot", (byte) i);
+				this.inventory[i].writeToNBT(nbttagcompound1);
+				nbttaglist.appendTag(nbttagcompound1);
+			}
+		}
+		nbt.setTag("Items", nbttaglist);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt)
+	{
+		super.readFromNBT(nbt);
+
+		NBTTagList nbttaglist = nbt.getTagList("Items", 10);
+
+		for (int i = 0; i < nbttaglist.tagCount(); ++i)
+		{
+			NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
+			int j = nbttagcompound1.getByte("Slot") & 255;
+
+			this.inventory[j] = ItemStack.loadItemStackFromNBT(nbttagcompound1);
+		}
+	}
+
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		NBTTagCompound nbt = new NBTTagCompound();
+		this.writeToNBT(nbt);
+		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 1, nbt);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt)
+	{
+		this.readFromNBT(pkt.func_148857_g());
 	}
 }
