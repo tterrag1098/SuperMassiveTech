e01b31d2cd8d4c5867c33f8d38418bb60b44b4e9
tterrag1098 <tterrag1098@gmail.com>
2014-03-24 12:21:34 -0400

    Bored in class. Alas, tooltips.

--- src/main/java/tterrag/supermassivetech/item/ItemStarHeart.java
+++ src/main/java/tterrag/supermassivetech/item/ItemStarHeart.java
@@ -1,10 +1,15 @@
 package tterrag.supermassivetech.item;
 
+import java.util.List;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import tterrag.supermassivetech.entity.item.EntityItemStarHeart;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemStarHeart extends ItemSMT
 {
@@ -17,7 +22,6 @@ public class ItemStarHeart extends ItemSMT
 	@Override
 	public Entity createEntity(World world, Entity location, ItemStack itemstack)
 	{
-
 		return new EntityItemStarHeart(world, location.posX, location.posY, location.posZ, itemstack, location.motionX, location.motionY, location.motionZ, ((EntityItem) location).delayBeforeCanPickup);
 	}
 
@@ -25,5 +29,15 @@ public class ItemStarHeart extends ItemSMT
 	public boolean hasCustomEntity(ItemStack stack)
 	{
 		return true;
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean par4) 
+	{
+		list.add("Creates a star when ignited in-world.");
+		list.add("More fire in the vicinity causes");
+		list.add("a higher chance for better stars.");
 	}
 }
