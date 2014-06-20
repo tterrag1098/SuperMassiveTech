53a6415b695265e1749ef68ddd9f11eef9b9dfcf
tterrag1098 <tterrag1098@gmail.com>
2014-06-20 00:56:41 -0400

    Decent enough template for star formation done. Now to make it real.

--- src/main/java/tterrag/supermassivetech/client/render/entity/RenderFormingStar.java
+++ src/main/java/tterrag/supermassivetech/client/render/entity/RenderFormingStar.java
@@ -4,14 +4,17 @@ import static org.lwjgl.opengl.GL11.*;
 
 import java.util.Random;
 
-import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL11;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
@@ -22,9 +25,10 @@ public class RenderFormingStar extends Render
 {
     private final ResourceLocation texture = new ResourceLocation(Reference.MOD_TEXTUREPATH, "textures/items/starHeart.png");
 
-    private int rot;
+    private float rot;
 
     private ItemStack stack;
+    private EntityItem item;
 
     private final Random rand = new Random();
 
@@ -32,9 +36,14 @@ public class RenderFormingStar extends Render
     public void doRender(Entity entity, double x, double y, double z, float var8, float var9)
     {
         if (stack == null)
+        {
             stack = new ItemStack(SuperMassiveTech.itemRegistry.heartOfStar);
+            item = new EntityItem(Minecraft.getMinecraft().theWorld, x, y, z, stack);
+        }
         
-        glPushMatrix();
+        rot = rot > 1000 ? 0 : rot;
+
+        item.setPosition(entity.posX, entity.posY, entity.posZ);
 
         glTranslatef((float) x, (float) y, (float) z);
 
@@ -42,11 +51,16 @@ public class RenderFormingStar extends Render
 
         bindTexture(TextureMap.locationItemsTexture);
         IIcon icon = stack.getItem().getIcon(stack, 0);
-        
+        GL11.glPushMatrix();
+
+        glScalef(0.75f, 0.75f, 0.75f);
+        glTranslatef(0, -1f/5f, 0);
         glDepthMask(true);
+        glTranslated(0, Math.sin(rot / 100) / 10, 0);
         glRotatef(rot % 360, 0, 1, 0);
-        ItemRenderer.renderItemIn2D(tessellator, icon.getMaxU(), icon.getMinV(), icon.getMinU(), icon.getMaxV(), icon.getIconWidth(), icon.getIconHeight(), 1 / 16f);
+        render3DItem(item, icon, tessellator);
         glRotatef(-(rot % 360), 0, 1, 0);
+        glTranslatef(0, 1f/5f, 0);
 
         glDisable(GL_TEXTURE_2D);
         glShadeModel(GL_SMOOTH);
@@ -54,10 +68,10 @@ public class RenderFormingStar extends Render
         glBlendFunc(GL_SRC_ALPHA, GL_ONE);
         glDisable(GL_ALPHA_TEST);
         glDisable(GL_CULL_FACE);
-        
+
         rand.setSeed(2983457L);
 
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i < rot / 20; i++)
         {
             glRotatef(rand.nextFloat() * 360.0F, 1.0F, 0.0F, 0.0F);
             glRotatef(rand.nextFloat() * 360.0F, 0.0F, 0.0F, 1.0F);
@@ -96,5 +110,41 @@ public class RenderFormingStar extends Render
     protected ResourceLocation getEntityTexture(Entity var1)
     {
         return null;
+    }
+
+    private void render3DItem(EntityItem entityItem, IIcon icon, Tessellator tessellator)
+    {
+        GL11.glPushMatrix();
+
+        float width = 1 / 16f;
+        float offset = 7 / 320f;
+        ItemStack itemstack = entityItem.getEntityItem();
+
+        float minU = icon.getMinU();
+        float maxU = icon.getMaxU();
+        float minV = icon.getMinV();
+        float maxV = icon.getMaxV();
+        float f7 = 0.5F;
+        float f8 = 0.25F;
+
+        int color = entityItem.getEntityItem().getItem().getColorFromItemStack(entityItem.getEntityItem(), 0);
+
+        GL11.glTranslatef(-f7, -f8, -(width + offset));
+
+        GL11.glTranslatef(0f, 0f, width + offset);
+
+        if (itemstack.getItemSpriteNumber() == 0)
+        {
+            this.bindTexture(TextureMap.locationBlocksTexture);
+        }
+        else
+        {
+            this.bindTexture(TextureMap.locationItemsTexture);
+        }
+
+        GL11.glColor4f(color & 8, color & 16, color & 24, 1.0F);
+        ItemRenderer.renderItemIn2D(tessellator, maxU, minV, minU, maxV, icon.getIconWidth(), icon.getIconHeight(), width);
+
+        GL11.glPopMatrix();
     }
 }
