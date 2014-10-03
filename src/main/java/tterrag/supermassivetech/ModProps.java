6185a60395f43d76132aa88eef7911b05fa68b07
tterrag1098 <tterrag1098@gmail.com>
2014-10-03 17:17:30 -0400

    Update to build 20 of ttCore and fix version strings.

--- src/main/java/tterrag/supermassivetech/ModProps.java
+++ src/main/java/tterrag/supermassivetech/ModProps.java
@@ -16,9 +16,9 @@ public class ModProps
     public static final String CHANNEL = "SMTech";
     
     // for dependencies
-    public static final String TTCORE_VERSION = "1.7.10-0.0.2-14,)";
-    public static final String FORGE_VERSION = "10.13.0.1171,)";
-    public static final String ENDERIO_VERSION = "1.7.10-2.0.186_beta,)";
+    public static final String TTCORE_VERSION = "[1.7.10-0.0.2-10,)";
+    public static final String FORGE_VERSION = "[10.13.0.1171,)";
+    public static final String ENDERIO_VERSION = "[1.7.10-2.0.186_beta,)";
     
     public static final String DEPENDENCIES = "required-after:Forge@" + FORGE_VERSION + ";"
                                             + "required-after:ttCore@" + TTCORE_VERSION + ";"
