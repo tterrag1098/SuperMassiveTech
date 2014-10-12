6b690ff352770ff09d9533a79cd3d3ca80eea3a3
tterrag1098 <tterrag1098@gmail.com>
2014-10-12 02:06:26 -0400

    Fix dependency replacement and update LICENSE

--- src/main/java/tterrag/supermassivetech/ModProps.java
+++ src/main/java/tterrag/supermassivetech/ModProps.java
@@ -7,21 +7,15 @@ public class ModProps
     public static final String MOD_NAME = "Super Massive Tech";
     public static final String LOCALIZING = "SMT";
     public static final String VERSION = "@VERSION@";
-    
+
     public static final String MAIN_PACKAGE = "tterrag.supermassivetech";
     public static final String CLIENT_PROXY_CLASS = MAIN_PACKAGE + ".client.ClientProxy";
     public static final String SERVER_PROXY_CLASS = MAIN_PACKAGE + ".common.CommonProxy";
     public static final String GUI_FACTORY_CLASS = MAIN_PACKAGE + ".common.config.ConfigFactorySMT";
-    
+
     public static final String CHANNEL = "SMTech";
-    
+
     // for dependencies
-    public static final String TTCORE_VERSION = "[MC1.7.10-0.0.2-20,)";
-    public static final String FORGE_VERSION = "[10.13.0.1171,)";
-    public static final String ENDERIO_VERSION = "[1.7.10-2.0.186_beta,)";
-    
-    public static final String DEPENDENCIES = "required-after:Forge@" + FORGE_VERSION + ";"
-                                            + "required-after:ttCore@" + TTCORE_VERSION + ";"
-                                            + "after:EnderIO@" + ENDERIO_VERSION + ";"
-                                            + "after:Waila;";
+
+    public static final String DEPENDENCIES = "after:ttCore;after:EnderIO"; // replaced by build file
 }
